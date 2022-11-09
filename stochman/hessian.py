20a035afe61c62622469d43c1525423a37242a45
Marco Miani <miani.1996@gmail.com>
2022-11-09 11:46:59 +0100

    added binary cross entropy hessian, and losses 0 and 1 order check

--- stochman/hessian.py
+++ stochman/hessian.py
@@ -17,7 +17,7 @@ class HessianCalculator(ABC, nn.Module):
         super().__init__()
 
         assert wrt in ("weight", "input")
-        assert loss_func in ("mse", "cross_entropy", "contrastive_pos", "contrastive_full", "contrastive_fix")
+        assert loss_func in ("mse", "cross_entropy_binary", "cross_entropy_multiclass", "contrastive_pos", "contrastive_full", "contrastive_fix")
         assert shape in ("full", "block", "diagonal")
         assert speed in ("slow", "half", "fast")
 
@@ -29,19 +29,68 @@ class HessianCalculator(ABC, nn.Module):
            # second order
             raise NotImplementedError
 
-    def compute_hessian(self, x, sequential, tuple_indices = None):
+    def compute_loss(self, x, target, nnj_module, tuple_indices = None):
 
         if self.loss_func == "mse":
-            return self.compute_mse_hessian(x, sequential)
-        elif self.loss_func == "cross_entropy":
-            return self.compute_cross_entropy_hessian(x, sequential)
+            return self.compute_mse(x, target, nnj_module)
+        elif "cross_entropy" in self.loss_func:
+            return self.compute_cross_entropy(x, target, nnj_module)
         elif "contrastive" in self.loss_func:
-            return self.compute_contrastive_hessian(x, sequential, tuple_indices)
+            return self.compute_contrastive(x, nnj_module, tuple_indices) #contrastive loss does not have targets
 
-    def compute_mse_hessian(self, x, sequential):
+    def compute_gradient(self, x, target, nnj_module, tuple_indices = None):
+
+        with torch.no_grad():
+            if self.loss_func == "mse":
+                return self.compute_mse_gradient(x, target, nnj_module)
+            elif "cross_entropy" in self.loss_func:
+                raise NotImplementedError
+            elif "contrastive" in self.loss_func:
+                raise NotImplementedError
+
+    def compute_hessian(self, x, nnj_module, tuple_indices = None):
+
+        with torch.no_grad():
+            if self.loss_func == "mse":
+                return self.compute_mse_hessian(x, nnj_module)
+            elif "cross_entropy" in self.loss_func:
+                return self.compute_cross_entropy_hessian(x, nnj_module)
+            elif "contrastive" in self.loss_func:
+                return self.compute_contrastive_hessian(x, nnj_module, tuple_indices)
+
+    
+    def compute_mse(self, x, target, nnj_module):
+
+        val = nnj_module(x)
+        assert val.shape == target.shape
+
+        # compute Gaussian log-likelihood
+        mse = 0.5*(val - target)**2
+
+        # average along batch size
+        mse = torch.mean(mse, dim=0)
+        return mse
+    
+    def compute_mse_gradient(self, x, target, nnj_module):
+
+        val = nnj_module(x)
+        assert val.shape == target.shape
+
+        # compute gradient of the Gaussian log-likelihood
+        gradient = val - target
+
+        # backpropagate through the network
+        gradient = nnj_module._vjp(x, val, gradient, 
+                                   wrt=self.wrt),
+
+        # average along batch size
+        gradient = torch.mean(gradient, dim=0)
+        return gradient
+
+    def compute_mse_hessian(self, x, nnj_module):
 
         # compute Jacobian sandwich of the identity for each element in the batch
-        Jt_J = sequential._jTmjp(x, None, None,
+        Jt_J = nnj_module._jTmjp(x, None, None,
                                  wrt = self.wrt, 
                                  to_diag = self.shape=="diagonal", 
                                  diag_backprop = self.speed=="fast")
@@ -49,14 +98,80 @@ class HessianCalculator(ABC, nn.Module):
         Jt_J = torch.mean(Jt_J, dim=0)
         return Jt_J
 
-    def compute_cross_entropy_hessian(self, x, sequential):
 
-        # init tmp
-        tmp = ...
+    def compute_cross_entropy(self, x, target, nnj_module):
 
-        return ...
+        val = nnj_module(x)
+        assert val.shape == target.shape
 
-    def compute_contrastive_hessian(self, x, sequential, tuple_indices):
+        if self.loss_func == "cross_entropy_binary":
+            bernoulli_p = torch.exp(val) / (1 + torch.exp(val))
+            cross_entropy = target * bernoulli_p + (1-target)*bernoulli_p
+
+        if self.loss_func == "cross_entropy_multiclass":
+            bernoulli_p = ...
+            cross_entropy = ...
+
+        # average along batch size
+        cross_entropy = torch.mean(cross_entropy, dim=0)
+        return cross_entropy
+
+    def compute_cross_entropy_hessian(self, x, nnj_module):
+
+        val = nnj_module(x)
+
+        # initialize the hessian H of the cross entropy
+        if self.loss_func == "cross_entropy_binary":
+            p = torch.exp(val) / (1 + torch.exp(val))
+            H = p - p**2  # hessian in diagonal form
+
+        if self.loss_func == "cross_entropy_multiclass":
+            H = ...
+
+        Jt_H_J = nnj_module._jTmjp(x, val, H,
+                                   wrt = self.wrt, 
+                                   from_diag = True,
+                                   to_diag = self.shape=="diagonal", 
+                                   diag_backprop = self.speed=="fast")
+        # average along batch size
+        Jt_H_J = torch.mean(Jt_H_J, dim=0)
+        return Jt_H_J
+
+    
+    def compute_contrastive(self, x, nnj_module, tuple_indices):
+        '''
+        Notice that the contrastive loss value is the same for 
+            self.loss_func == "contrastive_full"
+        and
+            self.loss_func == "contrastive_fix"
+        '''
+
+        # unpack tuple indices
+        if len(tuple_indices) == 3:
+            a, p, n = tuple_indices
+            ap = an = a
+        else:
+            ap, p, an, n = tuple_indices
+        assert len(ap)==len(p) and len(an)==len(n)
+
+        # compute positive part
+        pos = 0.5 * (nnj_module(x[ap]) - nnj_module(x[p]))**2
+
+        # sum along batch size
+        pos = torch.sum(pos, dim=0)
+
+        if self.loss_func == "contrastive_pos":
+            return pos
+        
+        # compute negative part
+        neg = 0.5 * (nnj_module(x[an]) - nnj_module(x[n]))**2
+
+        # sum along batch size
+        neg = torch.sum(neg, dim=0)
+        
+        return pos - neg
+
+    def compute_contrastive_hessian(self, x, nnj_module, tuple_indices):
 
         # unpack tuple indices
         if len(tuple_indices) == 3:
@@ -69,7 +184,7 @@ class HessianCalculator(ABC, nn.Module):
         if self.loss_func == "contrastive_full" or self.loss_func == "contrastive_pos":
 
             # compute positive part
-            pos = sequential._jTmjp_batch2(x[ap], x[p], None, None, None,
+            pos = nnj_module._jTmjp_batch2(x[ap], x[p], None, None, None,
                                            wrt = self.wrt,
                                            to_diag = self.shape=="diagonal", 
                                            diag_backprop = self.speed=="fast")
@@ -88,7 +203,7 @@ class HessianCalculator(ABC, nn.Module):
                 return pos
             
             # compute negative part
-            neg = sequential._jTmjp_batch2(x[an], x[n], None, None, None,
+            neg = nnj_module._jTmjp_batch2(x[an], x[n], None, None, None,
                                            wrt = self.wrt,
                                            to_diag = self.shape=="diagonal", 
                                            diag_backprop = self.speed=="fast")
@@ -111,7 +226,7 @@ class HessianCalculator(ABC, nn.Module):
             negatives = x[n] if len(tuple_indices) == 3 else torch.cat((x[an], x[n]))
 
             # compute positive part
-            pos = sequential._jTmjp(positives, None, None,
+            pos = nnj_module._jTmjp(positives, None, None,
                                     wrt = self.wrt, 
                                     to_diag = self.shape=="diagonal", 
                                     diag_backprop = self.speed=="fast")
@@ -119,7 +234,7 @@ class HessianCalculator(ABC, nn.Module):
             pos = torch.sum(pos, dim=0)
 
             # compute negative part
-            neg = sequential._jTmjp(negatives, None, None,
+            neg = nnj_module._jTmjp(negatives, None, None,
                                     wrt = self.wrt, 
                                     to_diag = self.shape=="diagonal", 
                                     diag_backprop = self.speed=="fast")
@@ -127,4 +242,3 @@ class HessianCalculator(ABC, nn.Module):
             neg = torch.sum(pos, dim=0)
 
             return pos - neg
-            
