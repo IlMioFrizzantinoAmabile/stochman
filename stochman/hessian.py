80b3b90593e0b119a9256b6513f7800e68beceb8
Marco Miani <miani.1996@gmail.com>
2022-11-23 10:08:35 +0100

    added arc cos loss hessian

--- stochman/hessian.py
+++ stochman/hessian.py
@@ -17,7 +17,10 @@ class HessianCalculator(ABC, nn.Module):
         super().__init__()
 
         assert wrt in ("weight", "input")
-        assert loss_func in ("mse", "cross_entropy_binary", "cross_entropy_multiclass", "contrastive_pos", "contrastive_full", "contrastive_fix")
+        assert loss_func in ("mse", 
+                             "cross_entropy_binary", "cross_entropy_multiclass", 
+                             "contrastive_full", "contrastive_pos", "contrastive_fix", 
+                             "arccos_full", "arccos_pos")
         assert shape in ("full", "block", "diagonal")
         assert speed in ("slow", "half", "fast")
 
@@ -37,6 +40,8 @@ class HessianCalculator(ABC, nn.Module):
             return self.compute_cross_entropy(x, target, nnj_module)
         elif "contrastive" in self.loss_func:
             return self.compute_contrastive(x, nnj_module, tuple_indices) #contrastive loss does not have targets
+        elif "arccos" in self.loss_func:
+            return self.compute_arccos(x, nnj_module, tuple_indices) #arccos loss does not have targets
 
     def compute_gradient(self, x, target, nnj_module, tuple_indices = None):
 
@@ -46,6 +51,8 @@ class HessianCalculator(ABC, nn.Module):
             elif "cross_entropy" in self.loss_func:
                 raise NotImplementedError
             elif "contrastive" in self.loss_func:
+                raise NotImplementedError
+            elif "arccos" in self.loss_func:
                 raise NotImplementedError
 
     def compute_hessian(self, x, nnj_module, tuple_indices = None):
@@ -57,6 +64,8 @@ class HessianCalculator(ABC, nn.Module):
                 return self.compute_cross_entropy_hessian(x, nnj_module)
             elif "contrastive" in self.loss_func:
                 return self.compute_contrastive_hessian(x, nnj_module, tuple_indices)
+            elif "arccos" in self.loss_func:
+                return self.compute_arccos_hessian(x, nnj_module, tuple_indices)
 
     
     def compute_mse(self, x, target, nnj_module):
@@ -144,6 +153,9 @@ class HessianCalculator(ABC, nn.Module):
     
     def compute_contrastive(self, x, nnj_module, tuple_indices):
         '''
+        L(x,y) = 0.5 * || x - y || 
+        Contrastive(x, tuples) = sum_positives L(x,y) - sum_negatives L(x,y)
+
         Notice that the contrastive loss value is the same for 
             self.loss_func == "contrastive_full"
         and
@@ -245,4 +257,140 @@ class HessianCalculator(ABC, nn.Module):
             # sum along batch size
             neg = torch.sum(pos, dim=0)
 
+            return pos - neg
+
+
+    def compute_arccos(self, x, nnj_module, tuple_indices):
+        '''
+        L(x,y) = 0.5 * sum_i x_i * y_i 
+               = 0.5 * || x / ||x|| - y / ||y|| || - 1    # arccos distance is equivalent to contrastive distance & normalization layer
+        Arcos(x, tuples) = sum_positives L(x,y) - sum_negatives L(x,y)
+        '''
+
+        def _arccos(x1, x2):
+            z1 = nnj_module(x1)
+            z2 = nnj_module(x2)
+            z1_norm = torch.sum( z1**2, dim=1)**(0.5)
+            z2_norm = torch.sum( z2**2, dim=1)**(0.5)
+            return 0.5 * torch.einsum("bi,bi->b", z1, z2) / (z1_norm * z2_norm)
+
+        # unpack tuple indices
+        if len(tuple_indices) == 3:
+            a, p, n = tuple_indices
+            ap = an = a
+        else:
+            ap, p, an, n = tuple_indices
+        assert len(ap)==len(p) and len(an)==len(n)
+
+        # compute positive part
+        pos = _arccos(x[ap], x[p]) 
+
+        # sum along batch size
+        pos = torch.sum(pos, dim=0)
+
+        if self.loss_func == "arccos_pos":
+            return pos
+        
+        # compute negative part
+        neg = _arccos(x[an], x[n])    
+
+        # sum along batch size
+        neg = torch.sum(neg, dim=0)
+        
+        return pos - neg
+
+    def compute_arccos_hessian(self, x, nnj_module, tuple_indices):
+
+        def _arccos_hessian(z1, z2):
+            z1_norm = torch.sum( z1**2, dim=1)**(0.5)
+            z2_norm = torch.sum( z2**2, dim=1)**(0.5)
+            z1_normalized = torch.einsum("bi,b->bi", z1, 1/z1_norm)
+            z2_normalized = torch.einsum("bi,b->bi", z2, 1/z2_norm)
+            cosine = torch.einsum("bi,bi->b", z1_normalized, z2_normalized)
+
+            zs = z1.shape
+            identity = torch.eye(zs[1], dtype=z1.dtype, device=z1.device).repeat(zs[0], 1, 1)
+            cosine_times_identity = torch.einsum("bij,b->bij", identity, cosine)
+
+            outer_11 = torch.einsum("bi,bj->bij", z1_normalized, z1_normalized)
+            outer_12 = torch.einsum("bi,bj->bij", z1_normalized, z2_normalized)
+            outer_21 = torch.einsum("bi,bj->bij", z2_normalized, z1_normalized)
+            outer_22 = torch.einsum("bi,bj->bij", z2_normalized, z2_normalized)
+        
+            cosine_times_outer_11 = torch.einsum("bij,b->bij", outer_11, cosine)
+            cosine_times_outer_21 = torch.einsum("bij,b->bij", outer_21, cosine)
+            cosine_times_outer_22 = torch.einsum("bij,b->bij", outer_22, cosine)
+
+            H_11 = cosine_times_identity + outer_12 + outer_21 - 3 * cosine_times_outer_11
+            H_12 = - identity + outer_11 + outer_22 - cosine_times_outer_21
+            H_22 = cosine_times_identity + outer_12 + outer_21 - 3 * cosine_times_outer_22
+
+            H_11_normalized = torch.einsum("bij,b->bij", H_11, 1/(z1_norm**2))
+            H_12_normalized = torch.einsum("bij,b,b->bij", H_12, 1/(z1_norm * z2_norm))
+            H_22_normalized = torch.einsum("bij,b->bij", H_22, 1/(z2_norm**2))
+            return tuple((H_11_normalized, H_12_normalized, H_22_normalized))
+
+
+        # unpack tuple indices
+        if len(tuple_indices) == 3:
+            a, p, n = tuple_indices
+            ap = an = a
+        else:
+            ap, p, an, n = tuple_indices
+        assert len(ap)==len(p) and len(an)==len(n)
+
+        if self.loss_func == "arccos_full" or self.loss_func == "arccos_pos":
+
+            # compute positive part
+
+            # forward pass 
+            z1, z2 = nnj_module(x[ap]), nnj_module(x[p])
+
+            # initialize the hessian of the loss
+            H = _arccos_hessian(z1, z2)
+
+            # backpropagate through the network
+            pos = nnj_module._jTmjp_batch2(x[ap], x[p], None, None, H,
+                                           wrt = self.wrt,
+                                           to_diag = self.shape=="diagonal", from_diag = False, 
+                                           diag_backprop = self.speed=="fast")
+            if self.shape=="diagonal":
+                if self.wrt == "weight":
+                    pos = [matrixes[0] - 2 * matrixes[1] + matrixes[2] for matrixes in pos]
+                    pos = torch.cat(pos, dim=1)
+                else:
+                    pos = pos[0] - 2 * pos[1] + pos[2]
+            else:
+                raise NotImplementedError
+            # sum along batch size
+            pos = torch.sum(pos, dim=0)
+
+
+            if self.loss_func == "contrastive_pos":
+                return pos
+            
+            # compute negative part
+
+            # forward pass 
+            z1, z2 = nnj_module(x[an]), nnj_module(x[n])
+
+            # initialize the hessian of the loss
+            H = _arccos_hessian(z1, z2)
+
+            # backpropagate through the network
+            neg = nnj_module._jTmjp_batch2(x[an], x[n], None, None, H,
+                                           wrt = self.wrt,
+                                           to_diag = self.shape=="diagonal", from_diag = False, 
+                                           diag_backprop = self.speed=="fast")
+            if self.shape=="diagonal":
+                if self.wrt == "weight":
+                    neg = [matrixes[0] - 2 * matrixes[1] + matrixes[2] for matrixes in neg]
+                    neg = torch.cat(neg, dim=1)
+                else:
+                    neg = neg[0] - 2 * neg[1] + neg[2]
+            else:
+                raise NotImplementedError
+            # sum along batch size
+            neg = torch.sum(neg, dim=0)
+            
             return pos - neg
